2c2fea78839373bf21ec839d0a65f251fece0713
Clay Oster <iamclayoster@gmail.com>
2025-07-05 08:40:09 -0500

    Try adding forwarded_allow_ips to show true source IP

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,7 @@ workers = 1
 accesslog = '-'
 errorlog = '-'
 #loglevel = 'debug'
+forwarded_allow_ips = '*'
 access_log_format = '%({x-forwarded-for}i)s %(l)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s'
 bind = ['0.0.0.0:8080']
 
